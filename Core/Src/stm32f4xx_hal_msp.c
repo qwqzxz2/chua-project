a14182f207eeacb025666ec432628f7e59ef52b0
qwqzxz2 <121784151+qwqzxz2@users.noreply.github.com>
2026-07-04 18:28:52 +0800

    Add Core/Src/stm32f4xx_hal_msp.c

---
+++ Core/Src/stm32f4xx_hal_msp.c
@@ -0,0 +1,101 @@
+#include "stm32f4xx_hal.h"
+
+extern DMA_HandleTypeDef hdma_usart1_tx;
+extern DMA_HandleTypeDef hdma_usart2_tx;
+
+void HAL_I2C_MspInit(I2C_HandleTypeDef *hi2c)
+{
+    GPIO_InitTypeDef gpio = {0};
+    if (hi2c->Instance == I2C1) {
+        __HAL_RCC_GPIOB_CLK_ENABLE();
+        __HAL_RCC_I2C1_CLK_ENABLE();
+        gpio.Pin = GPIO_PIN_6 | GPIO_PIN_7;
+        gpio.Mode = GPIO_MODE_AF_OD;
+        gpio.Pull = GPIO_PULLUP;
+        gpio.Speed = GPIO_SPEED_FREQ_HIGH;
+        gpio.Alternate = GPIO_AF4_I2C1;
+        HAL_GPIO_Init(GPIOB, &gpio);
+    }
+}
+
+void HAL_UART_MspInit(UART_HandleTypeDef *huart)
+{
+    GPIO_InitTypeDef gpio = {0};
+    if (huart->Instance == USART1) {
+        __HAL_RCC_USART1_CLK_ENABLE();
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        gpio.Pin = GPIO_PIN_9 | GPIO_PIN_10;
+        gpio.Mode = GPIO_MODE_AF_PP;
+        gpio.Pull = GPIO_PULLUP;
+        gpio.Speed = GPIO_SPEED_FREQ_HIGH;
+        gpio.Alternate = GPIO_AF7_USART1;
+        HAL_GPIO_Init(GPIOA, &gpio);
+        HAL_NVIC_SetPriority(USART1_IRQn, 5, 0);
+        HAL_NVIC_EnableIRQ(USART1_IRQn);
+    }
+    if (huart->Instance == USART2) {
+        __HAL_RCC_USART2_CLK_ENABLE();
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        gpio.Pin = GPIO_PIN_2 | GPIO_PIN_3;
+        gpio.Mode = GPIO_MODE_AF_PP;
+        gpio.Pull = GPIO_PULLUP;
+        gpio.Speed = GPIO_SPEED_FREQ_HIGH;
+        gpio.Alternate = GPIO_AF7_USART2;
+        HAL_GPIO_Init(GPIOA, &gpio);
+        HAL_NVIC_SetPriority(USART2_IRQn, 5, 0);
+        HAL_NVIC_EnableIRQ(USART2_IRQn);
+    }
+}
+
+void HAL_SPI_MspInit(SPI_HandleTypeDef *hspi)
+{
+    GPIO_InitTypeDef gpio = {0};
+    if (hspi->Instance == SPI1) {
+        __HAL_RCC_SPI1_CLK_ENABLE();
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        gpio.Pin = GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7;
+        gpio.Mode = GPIO_MODE_AF_PP;
+        gpio.Pull = GPIO_NOPULL;
+        gpio.Speed = GPIO_SPEED_FREQ_HIGH;
+        gpio.Alternate = GPIO_AF5_SPI1;
+        HAL_GPIO_Init(GPIOA, &gpio);
+    }
+}
+
+void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc)
+{
+    GPIO_InitTypeDef gpio = {0};
+    if (hadc->Instance == ADC1) {
+        __HAL_RCC_ADC1_CLK_ENABLE();
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        gpio.Pin = GPIO_PIN_0 | GPIO_PIN_1;
+        gpio.Mode = GPIO_MODE_ANALOG;
+        gpio.Pull = GPIO_NOPULL;
+        HAL_GPIO_Init(GPIOA, &gpio);
+    }
+}
+
+void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef *htim)
+{
+    GPIO_InitTypeDef gpio = {0};
+    if (htim->Instance == TIM3) {
+        __HAL_RCC_TIM3_CLK_ENABLE();
+        __HAL_RCC_GPIOB_CLK_ENABLE();
+        gpio.Pin = GPIO_PIN_1;
+        gpio.Mode = GPIO_MODE_AF_PP;
+        gpio.Pull = GPIO_NOPULL;
+        gpio.Speed = GPIO_SPEED_FREQ_LOW;
+        gpio.Alternate = GPIO_AF2_TIM3;
+        HAL_GPIO_Init(GPIOB, &gpio);
+    }
+    if (htim->Instance == TIM4) {
+        __HAL_RCC_TIM4_CLK_ENABLE();
+        __HAL_RCC_GPIOE_CLK_ENABLE();
+        gpio.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2;
+        gpio.Mode = GPIO_MODE_AF_PP;
+        gpio.Pull = GPIO_NOPULL;
+        gpio.Speed = GPIO_SPEED_FREQ_LOW;
+        gpio.Alternate = GPIO_AF2_TIM4;
+        HAL_GPIO_Init(GPIOE, &gpio);
+    }
+}
